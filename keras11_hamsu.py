927751eb870ec08a8efd89dc3a23e5483b9bf874
kyungtae92 <58333312+kyungtae92@users.noreply.github.com>
2019-12-04 23:08:43 +0900

    Add files via upload

---
+++ keras11_hamsu.py
@@ -0,0 +1,71 @@
+#1. 데이터
+import numpy as np
+
+x = np.array(range(1, 101))
+y = np.array(range(1, 101))
+print(x)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=66, test_size=0.4, shuffle=False) # 6:4
+x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, random_state=66, test_size=0.4, shuffle=False) # 6:2:2
+
+#2. 모델구성
+from keras.models import Sequential, Model
+from keras.layers import Dense, Input
+# model = Sequential()
+
+# input1 = Input(shape=(1,))
+# dense1 = Dense(5, activation='relu')(input1)
+# dense2 = Dense(3)(dense1)
+# dense3 = Dense(4)(dense2)
+# dense4 = Dense(4)(dense3)
+# dense5 = Dense(4)(dense4)
+# dense6 = Dense(4)(dense5)
+# dense7 = Dense(4)(dense6)
+# dense8 = Dense(4)(dense7)
+# dense9 = Dense(4)(dense8)
+# dense10 = Dense(4)(dense9)
+# output1 = Dense(1)(dense10)
+
+input1 = Input(shape=(1,))
+xx = Dense(5, activation='relu')(input1)
+xx = Dense(3)(xx)
+xx = Dense(4)(xx)
+xx = Dense(4)(xx)
+xx = Dense(4)(xx)
+xx = Dense(4)(xx)
+xx = Dense(4)(xx)
+xx = Dense(4)(xx)
+xx = Dense(4)(xx)
+xx = Dense(4)(xx)
+output1 = Dense(1)(xx)
+
+model = Model(inputs = input1, outputs = output1)
+model.summary()
+
+#3. 훈련
+# model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+# model.fit(x_train, y_train, epochs=100, batch_size=1)
+model.fit(x_train, y_train, epochs=100, batch_size=1, validation_data=(x_val, y_val)) # validation은 검증
+
+
+#4. 평가 예측
+loss, mse = model.evaluate(x_test, y_test, batch_size=1)  # a[0], a[1]
+print("mse : ", mse)    # 1.0
+
+y_predict = model.predict(x_test)
+print(y_predict)
+
+# RMSE 구하는 수식
+from sklearn.metrics import mean_squared_error
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+print("RMSE : ", RMSE(y_test, y_predict))
+
+from sklearn.metrics import r2_score
+r2_y_predict = r2_score(y_test, y_predict)
+print("R2 : ", r2_y_predict)
+
+# 레이어를 10개 이상 늘리시오.
+
